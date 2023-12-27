f9640b2bd9328d50e187037337a9b64637969206
Utsukta Khatri <utsukta99kc@gmail.com>
2023-12-27 17:51:00 +0545

    for loop with else added

---
+++ forelse.py
@@ -0,0 +1,13 @@
+#for else loop
+for i in range(3):
+    print(i)
+    
+    #here, when i becomes 2 then it will encounter break statement
+    #then, the loop will get terminated and else part will also not get executed.
+    if i==2:
+       break
+else:
+    print("hello") 
+
+
+
