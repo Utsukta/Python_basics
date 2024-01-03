c42f3ede5e8857804affcb01294a259c1b32b886
Utsukta Khatri <utsukta99kc@gmail.com>
2024-01-03 11:25:32 +0545

    writelines added

--- fileoperation.py
+++ fileoperation.py
@@ -2,7 +2,7 @@
 f=open("test.txt",'r')
 # print(f)
 fread= f.read()
-print(fread)
+# print(fread)
 
 #to open and write on the file= erase the already available content with given content.
 #also creates the file if not exists.
@@ -17,10 +17,32 @@ f.write(" Utsukta")
 #with statement
 with open('test.txt','a') as f:
     f.write(" is my name")
-print()
+
 
 
 #writeline takes objects such as list,tuples etc
 f=open("test.txt", 'w')
-lines=["line1","line2","line3"]
-f.writelines(lines)
+lines=["line1\n","line2\n","line3\n"]
+tupeles=(1,2,3,4,5,6,"hello")
+# f.writelines(lines)
+for i in tupeles:
+ print(i)
+ f.writelines(str(i))
+
+
+#readline
+f=open("test.txt","r")
+
+#this read only one line
+# print(f.readline()) 
+
+#to read all lines we will use while loop
+while True:
+    line=f.readline()
+    
+    if(not line ):
+      break
+    print(line)
+
+
+
