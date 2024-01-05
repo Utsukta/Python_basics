0cd3c1938ffd79c2fa439289c0f847b9e365b499
Utsukta Khatri <utsukta99kc@gmail.com>
2024-01-05 21:44:36 +0545

    Decorator Function Added

--- decoratorfunction.py
+++ decoratorfunction.py
@@ -1,18 +1,18 @@
-# import logging
+import logging
 
-# def log_function_call(func):
-#     def decorated(*args,**kwargs):
-#         logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
-#         result=func(*args, **kwargs)
-#         logging.info(f"{func.__name__} returned {result}")
-#         return result
-#     return decorated
+def log_function_call(func):
+    def decorated(*args,**kwargs):
+        logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+        result=func(*args, **kwargs)
+        logging.info(f"{func.__name__} returned {result}")
+        return result
+    return decorated
 
-# @log_function_call
-# def my_func(a,b):
-#     return a+b
+@log_function_call
+def my_func(a,b):
+    return a+b
 
-# my_func(2,3)
+my_func(2,3)
 
 
 # Without passing arguments
