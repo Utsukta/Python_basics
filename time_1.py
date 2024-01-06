5c008648837244f5b65dfacf2bbc2452f4fe867a
Utsukta Khatri <utsukta99kc@gmail.com>
2024-01-06 13:39:21 +0545

    Time Module updated

--- time_1.py
+++ time_1.py
@@ -4,3 +4,10 @@ timestamp=time.strftime("%H:%M:%S")
 print(timestamp)
 print(type(timestamp))
 
+init=time.time()
+for i in range(0,5000):
+    print(i)
+
+print(time.time()-init)
+time.sleep(5)
+print('after sleep')
